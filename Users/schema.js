d945ebabd937bfc2d48777c0c2d78e31b9ebc0b4
jslaybaugh <slaybaugh.j@northeastern.edu>
2025-12-09 15:18:53 -0500

    database connection

---
+++ Users/schema.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema({
+    _id: String,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    firstName: String,
+    lastName: String,
+    email: String,
+    phone: String,
+    dob: Date,
+    role: {
+      type: String,
+      enum: ["BUYER", "SELLER"],
+      default: "BUYER",
+    },
+    //loginId: String,
+  },
+  { collection: "users" }
+);
+export default userSchema;
---
+++ index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
+import "dotenv/config";
+import session from "express-session";
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/agora";
+mongoose.connect(CONNECTION_STRING);
+const app = express()
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+}));
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || "agora",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.SERVER_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.SERVER_URL,
+  };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+UserRoutes(app);
+app.listen(process.env.PORT || 4000);
